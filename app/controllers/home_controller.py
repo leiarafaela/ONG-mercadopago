7594f8257d838d3efe2f1f094348c411e8bd9a37
rafaela <rafaela20augusto@gmail.com>
2023-05-17 12:13:52 -0300

    metodo post

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import render_template
+from models.doador import Doador
 
-@app.route('/')
+@app.route('/home', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route('/home', methods=['POST'])
+def criar_doador():
+    infos_doador = {}
+    create_doador = Doador.create(infos_doador)
+
+    return create_doador
---
+++ app/models/doador.py
@@ -0,0 +1,19 @@
+from models.connection import cursor
+from contextlib import closing
+from util import row_to_dict, rows_to_dict
+
+class Doador():
+    def getAll():
+        with closing(cursor()) as con, closing(con.cursor()) as cur:
+            cur.execute("SELECT * FROM Doadores")
+            return rows_to_dict(cur.description, cur.fetchall())
+        
+    def create(doador):
+        with closing(cursor()) as con, closing(con.cursor()) as cur:
+            cur.execute("INSERT INTO Doadores (Nome, Sobrenome, Cidade, País, Valor) VALUES (%s, %s, %s, %s, %s)", [
+                doador['nome'], doador['sobrenome'], doador['cidade'], doador['pais'], doador['valor'],])
+            doador = cur.lastrowid
+            con.commit()
+            con.close()
+            return doador
+        
