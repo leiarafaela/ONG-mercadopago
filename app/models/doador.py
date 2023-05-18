a489c401d8fc4739cc4bd0d29a221f3762aae7c1
rafaela <rafaela20augusto@gmail.com>
2023-05-18 19:16:05 -0300

    Query que retorna lista de doadores

--- app/models/doador.py
+++ app/models/doador.py
@@ -6,7 +6,29 @@ import pyodbc
 
 class Doador():
     def getAll():
-        pass
+        try:
+        # Abrir uma nova conexão com o banco de dados
+            cursor = conexao.cursor()
+
+            # Tratar dados vazios
+            cursor.execute("UPDATE Doadores SET Nome = 'ANÔNIMO' WHERE Nome LIKE ''")
+            cursor.execute("UPDATE Doadores SET Sobrenome = 'ANÔNIMO' WHERE Sobrenome LIKE ''")
+            cursor.execute("UPDATE Doadores SET Cidade = 'ANÔNIMO' WHERE Cidade LIKE ''")
+            cursor.execute("UPDATE Doadores SET País = 'ANÔNIMO' WHERE País LIKE ''")
+            
+            cursor.execute("SELECT * FROM Doadores")
+
+            return rows_to_dict(cursor.description, cursor.fetchall())
+        
+        except pyodbc.Error as erro:
+        # Em caso de erro, desfazer as alterações
+            conexao.rollback()
+            print(f"Ocorreu um erro ao retornar os doadores: {erro}")
+
+        finally:
+            conexao.commit()
+            cursor.close()
+
         
     def create(doador):
         try:
